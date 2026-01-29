ecc10f9281145543d4b2f2c541f449e7962175f9
JavadAlip <javadalipp911@gmail.com>
2026-01-29 13:01:58 +0530

    Aminities icons added

---
+++ src/assets/Amenities/index.js
@@ -0,0 +1,43 @@
+import auditorium from './auditorium.png';
+import badminton from './badminton.png';
+import cctv from './cctv.png';
+import childrensArea from './childrens-area.png';
+import clubHouse from './club-house.png';
+import commonGarden from './common-garden.png';
+import communityHall from './community-hall.png';
+import fire from './fire.png';
+import gym from './gym.png';
+import indoorGames from './indoor-games.png';
+import joggingTrack from './jogging-track.png';
+import lift from './lift.png';
+import park from './park.png';
+import partyArea from './party-area.png';
+import powerBackup from './power-backup.png';
+import security from './security.png';
+import spa from './spa.png';
+import swimmingPool from './swimming-pool.png';
+import vastu from './vastu.png';
+import visitorsParking from './visitors-parking.png';
+
+export const AMENITY_ICONS = {
+  Auditorium: auditorium,
+  'Badminton Court': badminton,
+  'CCTV Camera': cctv,
+  'Childrens Play Area': childrensArea,
+  'Club House': clubHouse,
+  'Common Garden': commonGarden,
+  'Community Hall': communityHall,
+  'Fire Safety': fire,
+  Gym: gym,
+  'Indoor Games': indoorGames,
+  'Jogging Track': joggingTrack,
+  Lift: lift,
+  Park: park,
+  'Party Area': partyArea,
+  'Power Backup': powerBackup,
+  Security: security,
+  'Spa/Steam/Sauna': spa,
+  'Swimming Pool': swimmingPool,
+  'Vastu Compliant': vastu,
+  'Visitor parking': visitorsParking,
+};
