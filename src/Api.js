79c5487d0d9185ed4a9dde6455142b457f09220b
JavadAlip <javadalipp911@gmail.com>
2026-02-26 09:42:14 +0530

    Url changed to live

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_URL =
   import.meta.env.VITE_API_URL || 'https://upkar-backend.onrender.com/api';
-import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+// import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 //Admin Auth
 export const adminLoginApi = async (data) => {
