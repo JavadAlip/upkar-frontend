e5ccf992a093b9ca928d19ce6b105a5a4a151391
JavadAlip <javadalipp911@gmail.com>
2026-02-27 17:44:58 +0530

    User side scroll top top and footer changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,107 @@
+// import React, { useEffect, useState, useRef } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   useLocation,
+// } from 'react-router-dom';
+// import Footer from './Components/Common/Footer';
+// import { ChevronUp } from 'lucide-react';
+// import Home from './Pages/Home';
+// import AboutUs from './Pages/AboutUs';
+// import CompletedPrjcts from './Pages/CompletedPrjcts';
+// import UpcomingPrjcts from './Pages/UpcomingPrjcts';
+// import OngoingPrjcts from './Pages/OngoingPrjcts';
+// import ProjectDetail from './Pages/ProjectDetail';
+// import Event from './Pages/Event';
+// import Career from './Pages/Career';
+// import Blogs from './Pages/Blogs';
+// import Contact from './Pages/Contact';
+// import AdminDashboard from './AdminDashboard/Pages/AdminDashboard';
+// import AdminLogin from './AdminDashboard/Pages/AdminLogin';
+// import ProtectedRoute from './AdminDashboard/Components/ProtectedRoute';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import FloatingContact from './Components/Common/FloatingContact';
+
+// function AppWrapper() {
+//   const footerRef = useRef(null);
+//   const [showButton, setShowButton] = useState(false);
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (!footerRef.current) return;
+//       const footerTop = footerRef.current.getBoundingClientRect().top;
+//       const windowHeight = window.innerHeight;
+//       setShowButton(footerTop <= windowHeight);
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+//     return () => window.removeEventListener('scroll', handleScroll);
+//   }, []);
+
+//   const scrollToTop = () => {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+//   };
+
+//   const isAdminPage = location.pathname.startsWith('/admin');
+
+//   return (
+//     <>
+//       <Routes>
+//         {/* Public */}
+//         <Route path="/" element={<Home />} />
+//         <Route path="/aboutus" element={<AboutUs />} />
+//         <Route path="/completed-projects" element={<CompletedPrjcts />} />
+//         <Route path="/upcoming-projects" element={<UpcomingPrjcts />} />
+//         <Route path="/ongoing-projects" element={<OngoingPrjcts />} />
+//         <Route path="/project/:projectId" element={<ProjectDetail />} />{' '}
+//         <Route path="/events" element={<Event />} />
+//         <Route path="/careers" element={<Career />} />
+//         <Route path="/blogs" element={<Blogs />} />
+//         <Route path="/contact" element={<Contact />} />
+//         {/* Admin */}
+//         <Route path="/admin-login" element={<AdminLogin />} />
+//         <Route
+//           path="/admin/*"
+//           element={
+//             <ProtectedRoute>
+//               <AdminDashboard />
+//             </ProtectedRoute>
+//           }
+//         />
+//       </Routes>
+
+//       {!isAdminPage && <Footer ref={footerRef} />}
+//       {!isAdminPage && <FloatingContact />}
+//       {!isAdminPage && showButton && (
+//         // <button
+//         //   onClick={scrollToTop}
+//         //   className="fixed bottom-6 right-6 w-12 h-12 bg-white rounded-full shadow-lg flex items-center justify-center z-50"
+//         // >
+//         //   <ChevronUp className="w-6 h-6" />
+//         // </button>
+//         <button
+//           onClick={scrollToTop}
+//           className="fixed bottom-6 right-6 w-12 h-12 rounded-full  bg-white/30 backdrop-blur-md  border border-white/40  shadow-lg flex items-center justify-center  z-50 hover:bg-white/40 transition"
+//         >
+//           <ChevronUp className="w-6 h-6 text-white" />
+//         </button>
+//       )}
+//     </>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <ToastContainer />
+//       <AppWrapper />
+//     </Router>
+//   );
+// }
+
 import React, { useEffect, useState, useRef } from 'react';
 import {
   BrowserRouter as Router,
@@ -23,6 +127,8 @@ import ProtectedRoute from './AdminDashboard/Components/ProtectedRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import FloatingContact from './Components/Common/FloatingContact';
+import logo from './assets/logo.png';
+import ScrollToTop from './Components/Common/ScrollToTop';
 
 function AppWrapper() {
   const footerRef = useRef(null);
@@ -50,18 +156,19 @@ function AppWrapper() {
   return (
     <>
       <Routes>
-        {/* Public */}
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/completed-projects" element={<CompletedPrjcts />} />
         <Route path="/upcoming-projects" element={<UpcomingPrjcts />} />
         <Route path="/ongoing-projects" element={<OngoingPrjcts />} />
-        <Route path="/project/:projectId" element={<ProjectDetail />} />{' '}
+        <Route path="/project/:projectId" element={<ProjectDetail />} />
         <Route path="/events" element={<Event />} />
         <Route path="/careers" element={<Career />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/contact" element={<Contact />} />
-        {/* Admin */}
+
+        {/* Admin Routes */}
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route
           path="/admin/*"
@@ -75,16 +182,11 @@ function AppWrapper() {
 
       {!isAdminPage && <Footer ref={footerRef} />}
       {!isAdminPage && <FloatingContact />}
+
       {!isAdminPage && showButton && (
-        // <button
-        //   onClick={scrollToTop}
-        //   className="fixed bottom-6 right-6 w-12 h-12 bg-white rounded-full shadow-lg flex items-center justify-center z-50"
-        // >
-        //   <ChevronUp className="w-6 h-6" />
-        // </button>
         <button
           onClick={scrollToTop}
-          className="fixed bottom-6 right-6 w-12 h-12 rounded-full  bg-white/30 backdrop-blur-md  border border-white/40  shadow-lg flex items-center justify-center  z-50 hover:bg-white/40 transition"
+          className="fixed bottom-6 right-6 w-12 h-12 rounded-full bg-white/30 backdrop-blur-md border border-white/40 shadow-lg flex items-center justify-center z-50 hover:bg-white/40 transition"
         >
           <ChevronUp className="w-6 h-6 text-white" />
         </button>
@@ -94,8 +196,32 @@ function AppWrapper() {
 }
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  //  Splash Screen
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-white">
+        <img
+          src={logo}
+          alt="Upkar Logo"
+          className="w-40 sm:w-52 md:w-64 object-contain animate-pulse"
+        />
+      </div>
+    );
+  }
+
   return (
     <Router>
+      <ScrollToTop />
       <ToastContainer />
       <AppWrapper />
     </Router>
--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,133 +1,3 @@
-// import React, { forwardRef } from 'react';
-// import Footerimg from '../../assets/Footer.png';
-// import { FaLinkedin, FaWhatsapp, FaInstagram } from 'react-icons/fa';
-// import { FaXTwitter } from 'react-icons/fa6';
-// import { Link } from 'react-router-dom';
-
-// const Footer = forwardRef((props, ref) => {
-//   return (
-//     <footer
-//       ref={ref}
-//       className="w-full bg-[#000000] text-white py-10 lg:py-12 px-4 lg:px-10 font-[Figtree]"
-//     >
-//       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
-//         <div className="md:col-span-1 flex flex-col">
-//           {/* <p className="font-[Figtree] text-[20px] font-light leading-[1] text-white">
-//             Stay connected, explore opportunities,
-//             <br />
-//             and invest with confidence. Your real
-//             <br />
-//             estate success starts here.
-//           </p> */}
-//           <div className="flex gap-4 mt-6">
-//             <a href="#" className="hover:text-gray-300 transition-colors">
-//               <FaWhatsapp className="w-5 h-5 sm:w-6 sm:h-6" />
-//             </a>
-//             <a href="#" className="hover:text-gray-300 transition-colors">
-//               <FaInstagram className="w-5 h-5 sm:w-6 sm:h-6" />
-//             </a>
-//             <a href="#" className="hover:text-gray-300 transition-colors">
-//               <FaLinkedin className="w-5 h-5 sm:w-6 sm:h-6" />
-//             </a>
-//             <a href="#" className="hover:text-gray-300 transition-colors">
-//               <FaXTwitter className="w-5 h-5 sm:w-6 sm:h-6" />
-//             </a>
-//           </div>
-//         </div>
-
-//         <div className="md:col-span-1 md:ml-20 lg:ml-52">
-//           <ul className="space-y-2 font-[figtree] font-normal text-[17px]">
-//             <li>
-//               <Link
-//                 to="/aboutus"
-//                 className="hover:text-gray-300 transition-colors"
-//               >
-//                 About us
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/ongoing-projects"
-//                 className="hover:text-gray-300 transition-colors"
-//               >
-//                 Ongoing
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/upcoming-projects"
-//                 className="hover:text-gray-300 transition-colors"
-//               >
-//                 Upcoming
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/completed-projects"
-//                 className="hover:text-gray-300 transition-colors"
-//               >
-//                 Completed
-//               </Link>
-//             </li>
-//           </ul>
-//         </div>
-
-//         <div className="md:col-span-1 lg:ml-11">
-//           <ul className="space-y-2 inline-block text-left font-[Figtree] text-[17px] text-white">
-//             <li>
-//               <Link
-//                 to="/"
-//                 className="hover:text-gray-300 transition-colors font-normal"
-//               >
-//                 Commercial
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/events"
-//                 className="hover:text-gray-300 transition-colors font-normal"
-//               >
-//                 Events
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/blogs"
-//                 className="hover:text-gray-300 transition-colors font-normal"
-//               >
-//                 Blogs
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/careers"
-//                 className="hover:text-gray-300 transition-colors font-normal"
-//               >
-//                 Careers
-//               </Link>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-
-//       <div className="pt-8 flex flex-col items-center gap-8 lg:gap-12 w-full">
-//         <p className="text-center text-sm sm:text-base md:text-lg font-light">
-//           All Rights Reserved 2026 |{' '}
-//           <span className="font-bold">Upkar Group</span>
-//         </p>
-//         <img
-//           src={Footerimg}
-//           alt="Upkar Logo"
-//           className="w-full max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[60%] xl:max-w-[50%] object-contain"
-//         />
-//       </div>
-//     </footer>
-//   );
-// });
-
-// export default Footer;
-
-//new structure
 import React, { forwardRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FaLinkedin, FaWhatsapp, FaInstagram } from 'react-icons/fa';
@@ -144,7 +14,6 @@ const Footer = forwardRef((props, ref) => {
     >
       {/* TOP SECTION */}
       <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr_1fr_1fr] gap-10 lg:gap-6">
-        {/* LEFT LOGO + TEXT (MORE SPACE) */}
         <div>
           <img
             src={Footerimg}
@@ -152,7 +21,7 @@ const Footer = forwardRef((props, ref) => {
             className="mb-4 max-w-[180px] object-contain"
           />
 
-          <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-snug font-figtree text-white max-w-sm">
+          <p className="text-1xl sm:text-1xl md:text-2xl lg:text-2xl font-bold leading-snug font-figtree text-white max-w-sm">
             Shaping a 50+ years legacy of quality & trust !
           </p>
         </div>
@@ -206,17 +75,10 @@ const Footer = forwardRef((props, ref) => {
           </ul>
         </div>
 
-        {/* INFORMATION */}
         <div>
           <h4 className="text-lg font-semibold mb-2">Cities</h4>
           <hr className="w-40  border-white mb-4" />
           <ul className="space-y-2 text-white text-base">
-            {/* <li>
-              <Link to="/" className="hover:text-white">
-                Commercial
-              </Link>
-            </li> */}
-
             <li>
               <Link to="/" className="hover:text-white">
                 Projects in Bengaluru
@@ -230,7 +92,6 @@ const Footer = forwardRef((props, ref) => {
           </ul>
         </div>
 
-        {/* CONNECT */}
         <div>
           <h4 className="text-lg font-semibold mb-2">Connect</h4>
           <hr className="w-36  border-white mb-4" />
@@ -251,39 +112,25 @@ const Footer = forwardRef((props, ref) => {
         </div>
       </div>
 
-      {/* RIGHT SIDE LOGO ABOVE DIVIDER */}
       <div className="flex justify-end mb-6">
         <img
           src={Footerimg2}
           alt="Upkar Logo"
-          className="
-      w-[160px]
-      sm:w-[220px]
-      md:w-[300px]
-      lg:w-[220px]
-      xl:w-[220px]
-      2xl:w-[220px]
-      object-contain
-    "
+          className="w-[160px] sm:w-[220px]  md:w-[300px]  lg:w-[220px]  xl:w-[220px]  2xl:w-[220px] object-contain "
         />
       </div>
 
-      {/* DIVIDER */}
       <div className="border-t border-gray-700 my-10"></div>
-
-      {/* BOTTOM SECTION */}
       <div className="flex flex-col items-center gap-4">
-        {/* TEXT BELOW LOGO */}
-        <p className="text-center text-sm sm:text-base md:text-lg font-light">
-          Copyright © 2026, All rights reserved with{' '}
-          <span className="font-bold">Upkar Group</span>
-        </p>
-        {/* CENTER LOGO */}
         <img
           src={Footerimg1}
           alt="Upkar Logo"
           className="w-full max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[60%] xl:max-w-[50%] object-contain"
         />
+        <p className="text-center text-sm sm:text-base md:text-lg font-light">
+          Copyright © 2026, All rights reserved with{' '}
+          <span className="font-bold">Upkar Group</span>
+        </p>
       </div>
     </footer>
   );
